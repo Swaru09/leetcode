d43459f5f62dc89cf98b43601bc4a072e1dc7714
Swarnali Mukhopadhyay <100119142+Swaru09@users.noreply.github.com>
2024-08-15 11:09:37 +0530

    Time: 1 ms (73.7%), Space: 41.3 MB (62.8%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,10 +4,9 @@ class Solution {
         for(int i=0;i<strs[0].length();i++){
             char c=strs[0].charAt(i);
             for(int j=1;j<strs.length;j++){
-                if(i>=strs[j].length()||strs[j].charAt(i)!=c){
+                if (i>=strs[j].length()||strs[j].charAt(i)!=c){
                     return st.toString();
                 }
-
             }
             st.append(c);
         }
