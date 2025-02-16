ed81fe2e11bf3fc587a5859718fa68ee9356d0a7
Swarnali Mukhopadhyay <100119142+Swaru09@users.noreply.github.com>
2025-02-16 23:59:40 +0530

    Time: 9 ms (25.24%), Space: 40 MB (98.18%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,5 @@
 class Solution {
     public int fib(int n) {
-        int a=0;
-        int b=1;
         int s=0;
         if(n==0){
             return 0;
@@ -9,9 +7,7 @@ class Solution {
         if(n==1){
             return 1;
         }
-        if(n>=2){
-             s=fib(n-1)+fib(n-2);
-        }
+        s=fib(n-1)+fib(n-2);
         return s;
     }
 }
